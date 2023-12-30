46087b94e758f9a97ea07ac21166f55149e321b2
Zhiro990 <66927402+Zhiro990@users.noreply.github.com>
2023-12-30 15:41:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center"> { unit-converter } </h1>
+
+Unit converter in JavaScript.
+
+<h5 align="center"> Created with ❤ by Zhiro990 </h5>
---
+++ src/index.js
@@ -0,0 +1,5 @@
+exports.convertData = require("./functions/convertData.js");
+exports.convertLength = require("./functions/convertLength.js");
+exports.convertMass = require("./functions/convertMass.js");
+exports.convertTemperature = require("./functions/convertTemperature.js");
+exports.convertTime = require("./functions/convertTime.js");
